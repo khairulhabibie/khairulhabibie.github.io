b27da6b230c422d00ab7a7c241e8ee7729b8bd35
khairulhabibie <khairulhabibie01@gmail.com>
2022-07-15 21:20:40 +0700

    add nav menu

--- src/js/aside.js
+++ src/js/aside.js
@@ -1,27 +1,34 @@
 $(document).ready(() => {
+  // left-aside
   $('.left-open').click(function () {
+    // open
     $('#left-aside').css({
       animation: 'left-unhide-motion 0.3s forwards',
     });
   });
 
-  // left-aside
   $('.left-close').click(function () {
     $('#left-aside').css({
       animation: 'left-hide-motion 0.3s forwards',
     });
   });
 
+  // right-aside
   $('.right-open').click(function () {
+    // open
     $('#right-aside').css({
       animation: 'right-unhide-motion 0.3s forwards',
     });
   });
 
-  // right-aside
   $('.right-close').click(function () {
     $('#right-aside').css({
       animation: 'right-hide-motion 0.3s forwards',
     });
+  });
+
+  // center
+  $('.center-open').click(function () {
+    $('#content').children().slideDown();
   });
 });
