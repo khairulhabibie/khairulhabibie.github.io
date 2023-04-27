7b10e599b1d180cb8492d29a696581f75a5a9238
khairul habibie <96696443+khairulhabibie@users.noreply.github.com>
2023-04-27 23:48:25 +0000

    add style cover

--- src/Container/Cover/Cover.jsx
+++ src/Container/Cover/Cover.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import "./Cover.scss";
+// import "./Cover.scss";
 
 const Cover = () => {
   return (
-    <div id="cover" className="cover">
-      <div className="container ">
+    <div id="cover" className="flex h-screen pb-12">
+      <div className="m-auto">
         <img
           src="https://res.cloudinary.com/de8q1kwuh/image/upload/v1680297503/portofolio.khairulhabibie.my.id/khairulhabibie_lyxqwr.webp"
           alt="khairul_habibie_img"
         />
-        <h1>Khairul Habibie</h1>
-        <div className="sub-container">
+        <h1 className="text-center">Khairul Habibie</h1>
+        {/* <div className="sub-container">
           <a href="#about">About me</a>
           <a href="#skill">Skills</a>
           <a href="#projects">Projects</a>
           <a href="#messege">messege</a>
-        </div>
+        </div> */}
       </div>
     </div>
   );
--- src/Container/Footer.jsx
+++ src/Container/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-blue-950 py-5 text-2xl text-center ">
+    <footer className="bg-blue-950 py-5 text-2xl text-center">
       <p>Thank for View</p>
     </footer>
   );
