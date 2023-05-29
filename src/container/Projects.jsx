e15214159f7e96e0a6e6921d95b7a66289c8d844
khairul habibie <96696443+khairulhabibie@users.noreply.github.com>
2023-05-29 23:39:42 +0000

    membuat kostume hook useInput

--- src/container/Projects.jsx
+++ src/container/Projects.jsx
@@ -3,12 +3,12 @@ import ProjectItem from './ProjectItem';
 
 function Projects({ projects }) {
   return (
-    <div className="mx-4 mt-28 md:mx-24">
+    <div className="container mx-4 mt-28 md:mx-24">
       <div className="mb-4">
         <h1 className="mb-1 text-3xl font-bold">Projects</h1>
         <p>Showcase of my works on frontend development.</p>
       </div>
-      <div className="container w-full gap-3 mx-auto md:columns-3 sm:columns-2 ">
+      <div className="w-full gap-3 mx-auto md:columns-3 sm:columns-2">
         {projects.map((project) => {
           return <ProjectItem key={project.id} id={project.id} {...project} />;
         })}
