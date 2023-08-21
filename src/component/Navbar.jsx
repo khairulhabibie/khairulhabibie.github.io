0bfbe18c55493f3dd084cbbcce2ffe675998f88b
khairulhabibie <khairulhabibie01@gmail.com>
2023-08-21 12:14:39 +0700

    remove animation

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FaHome, FaBloggerB } from 'react-icons/fa';
-import { RiProfileFill } from 'react-icons/ri';
-import { AiFillProject } from 'react-icons/ai';
-import { IoMdLogIn } from 'react-icons/io';
-import { BiNotepad } from 'react-icons/bi';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaHome, FaBloggerB } from "react-icons/fa";
+import { RiProfileFill } from "react-icons/ri";
+import { AiFillProject } from "react-icons/ai";
+import { IoMdLogIn } from "react-icons/io";
+import { BiNotepad } from "react-icons/bi";
 
 function Navbar({ pathname, authed }) {
   // const navigate = useNavigate();
   const active = (data) => {
-    return pathname === data ? 'nav-btn-active' : 'nav-btn';
+    return pathname === data ? "nav-btn-active" : "nav-btn";
   };
   // function logOutHandler() {
   //   alert('The user has logged out successfully!');
@@ -18,13 +18,13 @@ function Navbar({ pathname, authed }) {
 
   if (authed === false) {
     return (
-      <nav className="fixed top-0 left-0 right-0 flex-wrap bg-black border-b md:justify-center md:flex">
+      <nav className="fixed top-0 left-0 right-0 flex-wrap bg-slate-950 border-b md:justify-center md:flex">
         <section className="hidden md:flex">
-          <Link to="/login" className={active('/login')}>
+          <Link to="/login" className={active("/login")}>
             <IoMdLogIn />
             <p>Login</p>
           </Link>
-          <Link to="/register" className={active('/register')}>
+          <Link to="/register" className={active("/register")}>
             <BiNotepad />
             <p>Register</p>
           </Link>
@@ -34,22 +34,22 @@ function Navbar({ pathname, authed }) {
   }
 
   return (
-    <nav className="fixed top-0 left-0 right-0 flex-wrap border-b md:justify-between md:flex">
+    <nav className="fixed top-0 left-0 right-0 flex-wrap bg-slate-950 border-b md:justify-between md:flex">
       <section className="static flex flex-col md:relative">
         <div className="flex flex-row flex-wrap">
-          <Link to="/" className={active('/')}>
+          <Link to="/" className={active("/")}>
             <FaHome />
             <p>Home</p>
           </Link>
-          <Link to="/project" className={active('/project')}>
+          <Link to="/project" className={active("/project")}>
             <AiFillProject />
             <p>Project</p>
           </Link>
-          <Link to="/blog" className={active('/blog')}>
+          <Link to="/blog" className={active("/blog")}>
             <FaBloggerB />
             <p>Blog</p>
           </Link>
-          <Link to="/about" className={active('/about')}>
+          <Link to="/about" className={active("/about")}>
             <RiProfileFill />
             <p>About</p>
           </Link>
