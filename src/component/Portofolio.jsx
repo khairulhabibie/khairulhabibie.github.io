a01885b8441faeb60ee012de01ce42300129fc48
khairulhabibie <khairulhabibie01@gmail.com>
2023-09-21 10:51:54 +0700

    add 2 project at project comp

--- src/component/Portofolio.jsx
+++ src/component/Portofolio.jsx
@@ -20,64 +20,37 @@ function Portofolio() {
         </div>
         <div className="w-full px-4 flex flex-wrap xl:w-10/12 xl:mx-auto">
           <div className="mb-12 p-4 md:w-1/2">
-            <div className="rounded-md shadow-md overflow-hidden">
-              <img
-                src="https://res.cloudinary.com/de8q1kwuh/image/upload/v1680297382/portofolio.khairulhabibie.my.id/A_Cafe_Menu_cf9qcv.webp"
-                alt="kedaikopi.webp"
-                className="w-full"
-              />
-            </div>
-            <h3 className="font-semibold text-xl text-dark mt-5 mb-3">
-              Khairul Habibie Landingpage
-            </h3>
+            <a href="https://bookshelf-v1.netlify.app/">
+              <div className="rounded-md shadow-md overflow-hidden md:max-h-[400px] max-h-[300px]">
+                <img
+                  src="https://res.cloudinary.com/de8q1kwuh/image/upload/v1695267971/projects/bookshelf-v1.webp"
+                  alt="bookshelf-v1"
+                  className="w-full"
+                  target="_blank"
+                />
+              </div>
+              <h3 className="font-semibold text-xl text-dark mt-5 mb-3">
+                Khairul Habibie Landingpage
+              </h3>
+            </a>
             <p className="font-medium text-base text-secondary">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius,
               sit soluta nam doloremque cupiditate dolores!
             </p>
           </div>
           <div className="mb-12 p-4 md:w-1/2">
-            <div className="rounded-md shadow-md overflow-hidden">
-              <img
-                src="https://res.cloudinary.com/de8q1kwuh/image/upload/v1680297382/portofolio.khairulhabibie.my.id/A_Cafe_Menu_cf9qcv.webp"
-                alt="kedaikopi.webp"
-                className="w-full"
-              />
-            </div>
-            <h3 className="font-semibold text-xl text-dark mt-5 mb-3">
-              Khairul Habibie Landingpage
-            </h3>
-            <p className="font-medium text-base text-secondary">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius,
-              sit soluta nam doloremque cupiditate dolores!
-            </p>
-          </div>
-          <div className="mb-12 p-4 md:w-1/2">
-            <div className="rounded-md shadow-md overflow-hidden">
-              <img
-                src="https://res.cloudinary.com/de8q1kwuh/image/upload/v1680297382/portofolio.khairulhabibie.my.id/A_Cafe_Menu_cf9qcv.webp"
-                alt="kedaikopi.webp"
-                className="w-full"
-              />
-            </div>
-            <h3 className="font-semibold text-xl text-dark mt-5 mb-3">
-              Khairul Habibie Landingpage
-            </h3>
-            <p className="font-medium text-base text-secondary">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius,
-              sit soluta nam doloremque cupiditate dolores!
-            </p>
-          </div>
-          <div className="mb-12 p-4 md:w-1/2">
-            <div className="rounded-md shadow-md overflow-hidden">
-              <img
-                src="https://res.cloudinary.com/de8q1kwuh/image/upload/v1680297382/portofolio.khairulhabibie.my.id/A_Cafe_Menu_cf9qcv.webp"
-                alt="kedaikopi.webp"
-                className="w-full"
-              />
-            </div>
-            <h3 className="font-semibold text-xl text-dark mt-5 mb-3">
-              Khairul Habibie Landingpage
-            </h3>
+            <a href="https://note-app-with-react-v11.netlify.app/">
+              <div className="rounded-md shadow-md overflow-hidden md:max-h-[400px] max-h-[300px]">
+                <img
+                  src="https://res.cloudinary.com/de8q1kwuh/image/upload/v1695267971/projects/note-app-with-react-v12.webp"
+                  alt="kedaikopi.webp"
+                  className="w-full"
+                />
+              </div>
+              <h3 className="font-semibold text-xl text-dark mt-5 mb-3">
+                Khairul Habibie Landingpage
+              </h3>
+            </a>
             <p className="font-medium text-base text-secondary">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius,
               sit soluta nam doloremque cupiditate dolores!
